ecfbad53ce01b6215d2cbe0a7d02c7ed0c00d246
Peter Us <peter.us.99@gmail.com>
2015-11-25 15:27:17 +0100

    Added .less stylesheet. Refactored directive code.

--- angular-ui-switch.js
+++ angular-ui-switch.js
@@ -5,24 +5,20 @@ angular.module('uiSwitch', [])
     restrict: 'AE'
   , replace: true
   , transclude: true
-  , template: function(element, attrs) {
-      var html = '';
-      html += '<span';
-      html +=   ' class="switch' + (attrs.class ? ' ' + attrs.class : '') + '"';
-      html +=   attrs.ngModel ? ' ng-click="' + attrs.disabled + ' ? ' + attrs.ngModel + ' : ' + attrs.ngModel + '=!' + attrs.ngModel + (attrs.ngChange ? '; ' + attrs.ngChange + '()"' : '"') : '';
-      html +=   ' ng-class="{ checked:' + attrs.ngModel + ', disabled:' + attrs.disabled + ' }"';
-      html +=   '>';
-      html +=   '<small></small>';
-      html +=   '<input type="checkbox"';
-      html +=     attrs.id ? ' id="' + attrs.id + '"' : '';
-      html +=     attrs.name ? ' name="' + attrs.name + '"' : '';
-      html +=     attrs.ngModel ? ' ng-model="' + attrs.ngModel + '"' : '';
-      html +=     ' style="display:none" />';
-      html +=     '<span class="switch-text">'; /*adding new container for switch text*/
-      html +=     attrs.on ? '<span class="on">'+attrs.on+'</span>' : ''; /*switch text on value set by user in directive html markup*/
-      html +=     attrs.off ? '<span class="off">'+attrs.off + '</span>' : ' ';  /*switch text off value set by user in directive html markup*/
-      html += '</span>';
-      return html;
-    }
+  , scope: {
+      ngModel: "=",
+      disabled: "="
+  }
+  , templateUrl: 'angular-ui-switch.html'
+  , controller: ["$scope", function($scope){
+      if (!$scope.disabled)
+        $scope.disabled = false;
+
+      $scope.onSwitch = function(){
+        if(!$scope.disabled){
+          $scope.ngModel = !$scope.ngModel;
+        }
+      }
+    }]
   }
 });
